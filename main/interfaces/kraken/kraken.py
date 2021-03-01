b10617bbe0d1a22d804921e26e8d789742875e6b
mario <mario.briseno7@gmail.com>
2021-02-28 21:24:32 -0600

    Implemented get_trades() method and refactored dictionary keys into variables. Added trade_file_name as class attribute

--- main/interfaces/kraken/kraken.py
+++ main/interfaces/kraken/kraken.py
@@ -18,6 +18,30 @@ from ..helpers import (
     check_file_existence,
 )
 
+columns_orders = {
+    "Id": "Id",
+    "pair": "Pair",
+    "vol": "Volume",
+    "opentm": "Opened at",
+    "order": "Description",
+}
+
+columns_trades = {
+    "Id": "Id",
+    "pair": "Pair",
+    "vol": "Volume",
+    "price": "Price",
+    "time": "Executed at",
+    "ordertxid": "From Order",
+    "fee": "Fee"
+}
+
+# Variables with needed keywords to access request's response dictionary.
+result, _open, pair = "result", "open", "pair"
+descr, vol, opened_at = "descr", "vol", "opentm"
+trades, price, time = "trades", "price", "time"
+order_id, fee, order = "ordertxid", "fee", "order"
+
 
 class KrakenAPI(object):
     """
@@ -28,6 +52,7 @@ class KrakenAPI(object):
         open_orders_file_name (str): File where open orders are stored
     """
 
+    trades_file_name = "kraken_trades.txt"
     balances_file_name = "kraken_balances.txt"
     open_orders_file_name = "kraken_open_orders.txt"
 
@@ -71,24 +96,28 @@ class KrakenAPI(object):
         response = self.api.query_private("OpenOrders")
         KrakenAPI.request_checker(response)
         formatted_response = dict()
-        columns = {
-            "Id": "Id",
-            "pair": "Pair",
-            "vol": "Volume",
-            "opentm": "Opened at",
-            "order": "Description",
-        }
-        for _id, item in response["result"]["open"].items():
+        for _id, item in response[result][_open].items():
             if _id not in formatted_response:
                 formatted_response[_id] = dict()
-            formatted_response[_id]["pair"] = item["descr"]["pair"]
-            formatted_response[_id]["vol"] = item["vol"]
-            formatted_response[_id]["opened_at"] = item["opentm"]
-            formatted_response[_id]["descr"] = item["descr"]["order"]
-        return tuple(columns.values()), formatted_response
+            formatted_response[_id][pair] = item[descr][pair]
+            formatted_response[_id][vol] = item[vol]
+            formatted_response[_id]["opened_at"] = item[opened_at]
+            formatted_response[_id][descr] = item[descr][order]
+        return tuple(columns_orders.values()), formatted_response
 
-    def get_trades(self):
-        pass
+    def get_trades(self) -> Tuple[Iterable, Dict[str, Dict[str, dict]]]:
+        response = self.api.query_private("TradesHistory")
+        KrakenAPI.request_checker(response)
+        formatted_response = dict()
+        for _id, item in response[result][trades].items():
+            if _id not in formatted_response:
+                formatted_response[_id] = dict()
+            formatted_response[_id][pair] = item[pair]
+            formatted_response[_id][vol] = item[vol]
+            formatted_response[_id][price] = item[price]
+            formatted_response[_id][time] = item[time]
+            formatted_response[_id][fee] = item[fee]
+        return tuple(columns_trades.values()), formatted_response
 
     @staticmethod
     def save(file_name, header, *columns, **response):
