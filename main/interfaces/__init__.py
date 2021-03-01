e6209de467a5ab143b7db32f99b16c5b17ba9b4a
mario <mario.briseno7@gmail.com>
2021-03-01 04:21:04 -0600

    added more imports to interface module since there are going to be needed at tests

--- main/interfaces/__init__.py
+++ main/interfaces/__init__.py
@@ -3,10 +3,17 @@ from .kraken import (
     KrakenAPI,
     ResponseException,
 )
+from .bitcoin import BitcoinAPI
+from .ethereum import EthereumAPI
 from .helpers import get_root_directory
+from .exceptions import (
+    BitcoinException,
+    EthereumException,
+    KrakenException,
+)
 from .interfaces import (
-    load_bitcoin_balances,
-    load_ethereum_adresseses_info,
     load_kraken_balances,
+    load_bitcoin_balances,
     load_kraken_open_orders,
+    load_ethereum_adresseses_info,
 )
